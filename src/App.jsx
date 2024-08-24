9f32510e9643dc0871703980f74ebfa78da5299d
PURNABRATA DEY <deypurnabrata2005@gmail.com>
2024-08-25 00:07:54 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+function App() {
+  const [color, setColor] = useState("olive");
+
+  return (
+    <div
+      className="w-full h-screen duration-200"
+      style={{ backgroundColor: color }}
+    >
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className=" flex flex-wrap justify-center gap-3 shadow-lg bg-black px-3 py-2 rounded-full">
+          <button
+            onClick={() => setColor("red")}
+            className="outline-none px-4 py-1 rounded-full text-white bg-red-500  shadow-lg"
+          >
+            Red
+          </button>
+          <button
+            onClick={() => setColor("green")}
+            className="outline-none px-4 py-1 rounded-full text-white bg-green-500  shadow-lg"
+          >
+            Green
+          </button>
+          <button
+            onClick={() => setColor("blue")}
+            className="outline-none px-4 py-1 rounded-full text-white bg-blue-500  shadow-lg"
+          >
+            Blue
+          </button>
+          <button
+            onClick={() => setColor("orange")}
+            className="outline-none px-4 py-1 rounded-full text-white bg-orange-500  shadow-lg"
+          >
+            Orange
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
